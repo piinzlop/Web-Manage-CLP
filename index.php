4b76de1f78735958a44fcb6e2c7d1b7dc15524eb
piinzlop <iceat_po@rmutsvmail.com>
2023-01-19 14:45:57 +0700

    เพิ่ม comment หน้า Index

--- index.php
+++ index.php
@@ -1327,30 +1327,28 @@ while ($row = mysqli_fetch_array($sql)) {
 
     </main><!-- End #main -->
 
-    <!-- ======= Footer ======= -->
-    <footer id="footer" class="footer">
-      <div class="copyright">
-        &copy; Copyright <strong><span>NiceAdmin</span></strong>. All Rights Reserved
-      </div>
-      <div class="credits">
-        <!-- All the links in the footer should remain intact. -->
-        <!-- You can delete the links only if you purchased the pro version. -->
-        <!-- Licensing information: https://bootstrapmade.com/license/ -->
-        <!-- Purchase the pro version with working PHP/AJAX contact form: https://bootstrapmade.com/nice-admin-bootstrap-admin-html-template/ -->
-        Designed by <a href="https://bootstrapmade.com/">BootstrapMade</a>
-      </div>
-    </footer><!-- End Footer -->
-
+    <!-- ปุ่มเลื่อนบนสุด -->
     <a href="#" class="back-to-top d-flex align-items-center justify-content-center"><i class="bi bi-arrow-up-short"></i></a>
 
-    <!-- Vendor JS Files -->
+    <!-- สคริปแผนภูมิ ApexCharts -->
     <script src="assets/vendor/apexcharts/apexcharts.min.js"></script>
+
+    <!-- สคริป Nabbar -->
     <script src="assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+
+    <!-- สคริป แผนภูมิ Charts -->
     <script src="assets/vendor/chart.js/chart.min.js"></script>
+
+    <!-- สคริป แผนภูมิ ECharts -->
     <script src="assets/vendor/echarts/echarts.min.js"></script>
+
     <script src="assets/vendor/quill/quill.min.js"></script>
+
+    <!-- สคริปกรองข้อมูลในตาราง -->
     <script src="assets/vendor/simple-datatables/simple-datatables.js"></script>
     <script src="assets/vendor/tinymce/tinymce.min.js"></script>
+
+    <!-- สคริปฟอร์มอีเมล -->
     <script src="assets/vendor/php-email-form/validate.js"></script>
 
     <!-- Template Main JS File -->
